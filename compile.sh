1da4a3087b0a6bbb12053fabdd65771f7f3415bb
Chris Wolfe <chriswwolfe@gmail.com>
2018-02-25 16:54:56 -0600

    include libseccomp

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -Wall --std=c99 $(pkg-config --cflags MagickWand) convert-w-sb.c -o convert-w-sb $(pkg-config --libs MagickWand) && ./convert-w-sb || true
+gcc -Wall --std=c99 $(pkg-config --cflags MagickWand libseccomp) convert-w-sb.c -o convert-w-sb $(pkg-config --libs MagickWand libseccomp) && ./convert-w-sb || true
 echo 'removing convert-w-sb'
 rm ./convert-w-sb
