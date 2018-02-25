dd7ebfe62e716cea08e721f7aad164093b788e16
Chris Wolfe <chriswwolfe@gmail.com>
2018-02-25 16:55:12 -0600

    start to use seccomp-bpf instead of the old

--- convert-w-sb.c
+++ convert-w-sb.c
@@ -11,49 +11,46 @@
 
 #include <linux/audit.h>
 #include <linux/filter.h>
-#include <linux/seccomp.h>
+
+#include <seccomp.h>
 
 #include <ImageMagick-6/wand/MagickWand.h>
 
-#define ArchField offsetof(struct seccomp_data, arch)
+int install_seccomp() {
+  int rc = -1;
 
-#define Allow(syscall)                                       \
-  BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_##syscall, 0, 1), \
-      BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW)
+  scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL);
+  rc = seccomp_arch_add(ctx, SCMP_ARCH_X86);
+  if (rc != 0) { goto out; };
+  rc = seccomp_arch_add(ctx, SCMP_ARCH_X86_64);
+  if (rc != 0) { goto out; };
 
-struct sock_filter filter[] = {
-    /* validate arch */
-    BPF_STMT(BPF_LD + BPF_W + BPF_ABS, ArchField),
-    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, AUDIT_ARCH_X86_64, 1, 0),
-    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+  // simple rules, break printf
+  rc = seccomp_load(ctx);
+  if (rc != 0) { goto out; };
 
-    /* load syscall */
-    BPF_STMT(BPF_LD + BPF_W + BPF_ABS, offsetof(struct seccomp_data, nr)),
+  return 0;
 
-    /* list of allowed syscalls, "the policy" */
-    Allow(exit_group), /* exits a processs */
-    Allow(brk),        /* for malloc(), inside libc */
-    Allow(mmap),       /* also for malloc() */
-    Allow(munmap),     /* for free(), inside libc */
-    Allow(write),      /* called by printf */
-    Allow(fstat),      /* called by printf */
-
-    /* and if we don't match above, die */
-    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
-};
-
-struct sock_fprog filterprog = {.len = sizeof(filter) / sizeof(filter[0]),
-                                .filter = filter};
+ out:
+  seccomp_release(ctx);
+  return -1;
+}
 
 int main(int argc, char **argv) {
   // setup the env
-  if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
-    perror("Could not start seccomp:");
-    exit(1);
-  }
-  // setup seccomp-bpf
-  if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &filterprog) == -1) {
-    perror("Could not start seccomp:");
+  //if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+  //  perror("Could not start seccomp:");
+  //  exit(1);
+  //}
+  //// setup seccomp-bpf
+  //if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, NULL) == -1) {
+  //  perror("Could not start seccomp:");
+  //  exit(1);
+  //}
+
+  int ret = install_seccomp();
+  if (ret != 0) {
+    printf("hi\n");
     exit(1);
   }
 
