59ba50dbd5abf4fe93110a930330e7e8a437ebb0
Chris Wolfe <chriswwolfe@gmail.com>
2018-02-25 15:14:32 -0600

    compile, remove

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-gcc -Wall --std=c99 $(pkg-config --cflags ImageMagick) convert-w-sb.c -o convert-w-sb $(pkg-config --libs MagickWand)
+#!/bin/bash
+
+gcc -Wall --std=c99 $(pkg-config --cflags MagickWand) convert-w-sb.c -o convert-w-sb $(pkg-config --libs MagickWand) && ./convert-w-sb || true
+echo 'removing convert-w-sb'
+rm ./convert-w-sb
